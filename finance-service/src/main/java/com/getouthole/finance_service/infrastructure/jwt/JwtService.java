609e76618d0c9e244ce03e59997441e53f50d0a9
AndrewPolengolas <andrewnd2009@outlook.com>
2025-05-03 22:50:13 -0300

    Finance service

---
+++ finance-service/src/main/java/com/getouthole/finance_service/infrastructure/jwt/JwtService.java
@@ -0,0 +1,62 @@
+package com.getouthole.finance_service.infrastructure.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class JwtService {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+
+    private Key getSignKey() {
+        return Keys.hmacShaKeyFor(secretKey.getBytes());
+    }
+
+
+    public Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public String extractUsername(String token){
+        return extractAllClaims(token).get("email", String.class);
+    }
+
+    public String extractId(String token){
+        return extractAllClaims(token).getSubject();
+    }
+
+    public boolean isTokenValid(String token) {
+        try {
+            return !isTokenExpired(token);
+        }catch (Exception e){
+            return false;
+        }
+    }
+
+    public List<String> extractRoles(String token){
+        return extractAllClaims(token).get("roles", List.class);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getExpiration()
+                .before(new Date());
+    }
+}
